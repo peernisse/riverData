eda882e559aa3ee57672513febe104d8242f1fdb
Peter EerNisse Surface Pro <peernisse@gmail.com>
2018-07-22 16:42:03 -0600

    Create helpers file to hold global variables and functions.

---
+++ helpers.R
@@ -0,0 +1,13 @@
+# Recreation.gov API URLs--------------------------------------
+# Set login varibles for Recreation.gov API
+usr<-"peernisse@gmail.com"
+pwd<-"5thStreet"
+key<-"80E50F3D830B46D4BC43DDF5592DE6B6"
+
+# Import entrance ids
+
+pathEntID<-"https://ridb.recreation.gov/api/v1/permitentrances"
+
+
+#Entrance IDs of interest
+ids<-c(376,377,378,379,380,371)
